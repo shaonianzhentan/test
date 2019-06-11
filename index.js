01fd98b45bc32d4cfee44ae11a2e0623d1255743
zhulu <shaonianzhentan@sina.cn>
2019-06-11 14:23:46 +0800

    测试

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
+var express = require('express');
+var httpProxyMiddleware = require('http-proxy-middleware');
+
 var proxy = new httpProxy.createProxyServer({
     target: 'ws://localhost:46514',
     ws: true
 });
 var proxyServer = http.createServer(function (req, res) {
     setTimeout(function () {
-        if (req.url.indexOf('/api') == 0) {
-            proxy.web(req, res, {
-                target: 'http://jiluxinqing.000webhostapp.com'
-            });
-        }else{
-            proxy.web(req, res, {
-                target: 'http://localhost:9008'
-            });
-        }        
+        proxy.web(req, res, {
+            target: 'http://localhost:9008'
+        });
     }, 500);
 });
 
@@ -31,8 +28,22 @@ proxyServer.listen(80);
 //
 // Create your target server
 //
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
-    res.end();
-}).listen(9008);
+
+var app = express();
+
+app.use(
+    '/api',
+    proxy({ target: 'http://jiluxinqing.000webhostapp.com', changeOrigin: true })
+);
+
+app.get('/', (req, res) => {
+    res.send('123456')
+})
+
+app.listen(9008);
+
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
+//     res.end();
+// }).listen(9008);
