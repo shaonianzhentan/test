7051238326c88672e0456d5b6adf10217006cb60
zhulu <shaonianzhentan@sina.cn>
2019-07-04 21:36:43 +0800

    加入反向代理

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
-var express = require('express');
-var httpProxyMiddleware = require('http-proxy-middleware');
 
 var proxy = new httpProxy.createProxyServer({
-    target: 'ws://localhost:46514',
+    target: 'ws://129.28.65.118:8123',
     ws: true
 });
 var proxyServer = http.createServer(function (req, res) {
-    setTimeout(function () {
-        proxy.web(req, res, {
-            target: 'http://localhost:9008'
-        });
-    }, 500);
+    proxy.web(req, res, {
+        target: 'http://129.28.65.118:8123'
+    });
 });
 
 //
@@ -23,27 +19,8 @@ proxyServer.on('upgrade', function (req, socket, head) {
     proxy.ws(req, socket, head);
 });
 
+proxy.on('error', function(e) {
+    console.log(e)
+});
+
 proxyServer.listen(80);
-
-//
-// Create your target server
-//
-
-var app = express();
-
-app.use(
-    '/api',
-    httpProxyMiddleware({ target: 'http://jiluxinqing.000webhostapp.com', changeOrigin: true })
-);
-
-app.get('/', (req, res) => {
-    res.send('123456')
-})
-
-app.listen(9008);
-
-// http.createServer(function (req, res) {
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
-//     res.end();
-// }).listen(9008);
