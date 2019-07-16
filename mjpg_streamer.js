c5ddcc32f3a5a7f8d37f2c76646c2ace9f92b44d
shaonianzhentan <ilovevar@gmail.com>
2019-07-16 23:08:59 +0800

    test

--- mjpg_streamer.js
+++ mjpg_streamer.js
@@ -2,3 +2,7 @@ const child_process = require('child_process')
 
 child_process.spawnSync(`LD_LIBRARY_PATH=/usr/local/lib  mjpg_streamer -i "input_uvc.so" -o "output_http.so -w /usr/local/www"`)
 console.log(`${new Date().toLocaleString()}开启服务`)
+
+setInterval(() => {
+    console.log('test')
+}, 80000)
