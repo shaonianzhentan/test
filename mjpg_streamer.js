790e60e739d3538cfbb42ce9cf74bfd6d3687cb1
shaonianzhentan <ilovevar@gmail.com>
2019-07-16 23:15:09 +0800

    test

--- mjpg_streamer.js
+++ mjpg_streamer.js
@@ -1,8 +1,15 @@
-const child_process = require('child_process')
+const { exec } = require('child_process')
 
-child_process.spawnSync(`LD_LIBRARY_PATH=/usr/local/lib  mjpg_streamer -i "input_uvc.so" -o "output_http.so -w /usr/local/www"`)
+exec(`LD_LIBRARY_PATH=/usr/local/lib  mjpg_streamer -i "input_uvc.so" -o "output_http.so -w /usr/local/www"`, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`执行的错误: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+})
 console.log(`${new Date().toLocaleString()}开启服务`)
 
 setInterval(() => {
     console.log('test')
-}, 80000)
+}, 8000000000)
